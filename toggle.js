3c11f00579d6dff7fe0d1499e311360bbb797af7
qDread-dev <58754377+qDread-dev@users.noreply.github.com>
2023-05-18 19:09:41 +0000

    Made some major changes

---
+++ toggle.js
@@ -0,0 +1,15 @@
+/* Made by Rob Diaz
+
+For more animations visit https://www.robdiaz.com/
+Contact: hello@robdiaz.com
+
+*/
+
+var animData = {
+    container: document.getElementById('container'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: ''
+};
+var anim = bodymovin.loadAnimation(animData);
